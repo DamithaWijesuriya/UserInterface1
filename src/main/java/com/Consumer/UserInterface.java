14a436367441d764a4b5715826470c5d6a874006
Damitha <damitha@hsenidlanka.com>
2016-09-26 10:20:18 +0530

    Initial Commit

---
+++ src/main/java/com/Consumer/UserInterface.java
@@ -0,0 +1,21 @@
+package com.Consumer;
+import com.Services.Patterns;
+
+
+
+/**
+ * Created by hsenid on 9/21/16.
+ */
+public class UserInterface {
+    /**
+     *
+     * Main method
+     */
+    public static void main(String args[])
+    {
+
+        Patterns.switchans("17");
+
+    }
+
+}
